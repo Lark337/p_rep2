499136d4f8df1d9ab52857a029c7536d930b5864
Lark <vkostroff98@gmail.com>
2023-12-16 18:57:18 +0300

    1st added

---
+++ d1/p1.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/clothes/")
+def clothes():
+    return render_template("clothes.html")
+
+@app.route("/shoes/")
+def shoes():
+    return render_template("shoes.html")
+
+@app.route("/contacts/")
+def contacts():
+    return render_template("contacts.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
